4e8599faa0efca886dbb7f54e8bdfdaac78ee6e7
kholmetskii <150633190+kholmetskii@users.noreply.github.com>
2024-09-06 17:23:11 +0300

    Added ability to see the entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+import markdown
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +9,24 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content is None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+
+def entry(request, title):
+    html_content = convert_md_to_html(title)
+    if html_content is None:
+        return render(request, "encyclopedia/not_found.html", {
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
